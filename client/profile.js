c0367e2fb5f8fc0d32a94b5ba52eef0e9a4aecbc
Josh Owens <joshua.owens@gmail.com>
2015-01-29 13:26:43 -0500

    Get tweets chronological on the profile and stream pages. Fixes #6

--- client/profile.js
+++ client/profile.js
@@ -10,5 +10,8 @@ Template.profile.events({
 Template.profile.helpers({
   following: function() {
     return _(Meteor.user().profile.followingIds).contains(this.user._id);
+  },
+  tweets: function() {
+    return Tweets.find({userId: this.user._id}, {sort: {tweetedAt: -1}});
   }
 });
