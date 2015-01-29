636923b579a6b5a9b5f046bfe87ebb4f214bc107
Josh Owens <joshua.owens@gmail.com>
2015-01-29 12:48:48 -0500

    Get the follow button working. Refs #5

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('tweets', function() {
   return Tweets.find();
 });
+
+Meteor.publish('profile', function(username) {
+  return Meteor.users.find({username: username}, {fields: {emails: 0, services: 0}});
+});
