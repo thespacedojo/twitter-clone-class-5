1b389b7e4733298bda4f910905ece005bc7f7e4e
Josh Owens <joshua.owens@gmail.com>
2015-01-29 12:08:19 -0500

    Get the tweet stream page functional. Fixes #4

---
+++ client/tweet_stream.js
@@ -0,0 +1,15 @@
+Template.tweetStream.helpers({
+  tweetedTime: function() {
+    return moment(this.tweetedAt).fromNow();
+  }
+});
+
+Template.tweetStream.events({
+  "submit form": function(event, template) {
+    event.preventDefault();
+    tweet = template.$('.tweet-text').val();
+    Tweets.insert({text: tweet}, function(){
+      template.$('.tweet-text').val(null);
+    });
+  }
+});
---
+++ collections/tweets.js
@@ -0,0 +1,33 @@
+Tweets = new Mongo.Collection('tweets');
+
+Tweets.before.insert(function(userId, doc) {
+  doc.tweetedAt = new Date();
+  doc.userId = userId;
+});
+
+Tweets.helpers({
+  user: function() {
+    return Meteor.users.findOne({_id: this.userId});
+  },
+  fullName: function() {
+    if (this.user() && this.user().profile)
+      return this.user().profile.name;
+  },
+  username: function() {
+    if (this.user)
+      return this.user().username;
+  }
+});
+
+Tweets.allow({
+  insert: function(userId, doc) {
+    if (userId)
+      return true;
+  },
+  update: function(userId, doc, fields, modifier) {
+    return false;
+  },
+  remove: function(userId, doc) {
+    return doc.userId === userId;
+  }
+});
